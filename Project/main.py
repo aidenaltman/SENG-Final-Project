729d47ae4d16e4c6e5305e3597833fe24e4f878f
Aiden Altman <aiden.altman@gmail.com>
2024-04-18 12:48:37 -0400

    Code clean up, removed test print statements

--- Project/main.py
+++ Project/main.py
@@ -1,5 +1,4 @@
-from Node import Node
-from LinkedList import LinkedList
+from LinkedList import LinkedList, Node
 from sortDLL import sort_linked_list
 
 userChoice = None
@@ -8,18 +7,16 @@ def printList(taskList):
     node = taskList.head
     if node == None:
         print()
-        print("No Tasks")
+        print("No tasks")
     else:
         print("--------------------------------------------------------")
         num = 1
         while node != None:
             print(f'{num}. {node.data[0]}')
-            print(f'\tDifficulty: {node.data[1]} Time: {node.data[2]}')
+            print(f'\tDifficulty: {node.data[1]} Time: {node.data[2]} minutes')
             num +=1
             node = node.next
         print("--------------------------------------------------------")
-
-
 
 
 while userChoice not in ('q', 'quit', 'exit', "5"):
@@ -30,9 +27,8 @@ while userChoice not in ('q', 'quit', 'exit', "5"):
     print('3. Remove task')
     print('4. Sort')
     print('5. Quit')
+    print("--------------------------------------------------------")
     userChoice = input().lower()
-    #print(userChoice)
-    
     
     if userChoice in ("print", "print list", "1"):
         printList(taskList) 
@@ -42,51 +38,62 @@ while userChoice not in ('q', 'quit', 'exit', "5"):
         print("Name of task:")
         taskName = input()
         print("Task difficulty (Out of 10):")
-        taskDiff = input()
+        while True:
+            taskDiff = input()
+            try: 
+                taskDiff = int(taskDiff)
+                break
+            except ValueError:
+                pass
+            print("Please input an integer")
+        
         print("Task time (Minutes):")
-        taskTime = input()
+        while True:
+            taskTime = input()
+            try: 
+                taskTime = int(taskTime)
+                break
+            except ValueError:
+                pass
+            print("Please input an integer")
         
         node = Node((taskName, int(taskDiff), int(taskTime)))
         taskList.append(node)
-
-        # node = Node(("test1", 8, 110))
-        # taskList.append(node)
-        # node = Node(("test2", 3, 40))
-        # taskList.append(node)
-        # node = Node(("test3", 6, 80))
-        # taskList.append(node)
-        # node = Node(("test4", 2, 60))
-        # taskList.append(node)
-        # taskList.append(node1)
-        # taskList.append(node2)
-        # taskList.append(node3)
+    
 
     elif userChoice in ("remove task", "remove", "3"):
-        print("Name of task to remove:")
-        printList(taskList)
-        taskRemove = input().lower()
-        node = taskList.head
-        
-        while taskRemove != node.data[0].lower():
-            if node.next == None:
-                print("That task is not on the list.")
-                break
-            node = node.next
-        if node.data[0] == taskRemove:
-            taskList.remove(node)
+        if taskList.head == None:
+            print()
+            print("No tasks to remove")
+        else:
+            print()
+            print("Name of task to remove:")
+            printList(taskList)
+            taskRemove = input().lower()
+            node = taskList.head
             
+            while taskRemove != node.data[0].lower():
+                if node.next == None:
+                    print("That task is not on the list.")
+                    break
+                node = node.next
+            if node.data[0] == taskRemove:
+                taskList.remove(node)
+                
         
     elif userChoice in ("sort", "4"):
-        print('Sort by difficulty or time?')
-        taskSort = input().lower()
-        if taskSort in ("diff", "difficulty"):
-            sortList = taskList
-            sortedList = sort_linked_list(sortList, 1)
-            printList(sortedList)
+        if taskList.head == None:
+            print("No tasks to sort")
+        else:
+        
+            print('Sort by difficulty or time?')
+            taskSort = input().lower()
+            if taskSort in ("diff", "difficulty"):
+                sortList = taskList
+                sortedList = sort_linked_list(sortList, 1)
+                printList(sortedList)
 
-        elif taskSort == "time":
-            sortList = taskList
-            sortedList = sort_linked_list(sortList, 2)
-            printList(sortedList)
-            
-
+            elif taskSort == "time":
+                sortList = taskList
+                sortedList = sort_linked_list(sortList, 2)
+                printList(sortedList)
--- Project/sortDLL.py
+++ Project/sortDLL.py
@@ -1,5 +1,4 @@
-from Node import Node
-from LinkedList import LinkedList
+from LinkedList import LinkedList, Node
 
 def partition(start, end, num):
     pivot = end.data
@@ -15,9 +14,7 @@ def partition(start, end, num):
     p_index.data, end.data = end.data, p_index.data
     return p_index
 def quicksort(start, end, num):
-    if start is None or end is None or start == end or start == end.next:
-        return start, end
-
+    
     pivot = partition(start, end, num)
 
     if pivot.prev:
@@ -37,8 +34,6 @@ def quicksort(start, end, num):
     return left_head, right_tail
 
 def sort_linked_list(linked_list, num):
-    if linked_list.head is None or linked_list.head == linked_list.tail:
-        return linked_list
 
     sorted_head, sorted_tail = quicksort(linked_list.head, linked_list.tail, num)
     sorted_list = LinkedList()
@@ -46,70 +41,3 @@ def sort_linked_list(linked_list, num):
     sorted_list.tail = sorted_tail
 
     return sorted_list
-
-    
-
-# testList = LinkedList()
-# node1 = Node(("test1", 8, 110))
-# #taskList.append(node)
-# node2 = Node(("test2", 3, 40))
-# #taskList.append(node)
-# node3 = Node(("test3", 6, 80))
-# #taskList.append(node)
-# node4 = Node(("test4", 5, 60))
-# testList.append(node1)
-# testList.append(node2)
-# testList.append(node3)
-# testList.append(node4)
-
-# def printList(taskList):
-#     node = taskList.head
-#     if node == None:
-#         print()
-#         print("No Tasks")
-#     else:
-#         print("--------------------------------------------------------")
-#         num = 1
-#         while node != None:
-#             print(f'{num}. {node.data[0]}')
-#             print(f'\tDifficulty: {node.data[1]} Time: {node.data[2]}')
-#             num +=1
-#             node = node.next
-#         print("--------------------------------------------------------")
-
-# printList(testList)
-# sortedList = sort_linked_list(testList, 2)
-# printList(sortedList)
-
-
-
-
-
-
-
-
-
-
-# def print_linked_list(linked_list):
-#     current = linked_list.head
-#     while current:
-#         print(current.data, end=" ")
-#         current = current.next
-#     print()
-
-# Example usage:
-# Creating a linked list
-# ll = LinkedList()
-# ll.append(Node(3))
-# ll.append(Node(2))
-# ll.append(Node(5))
-# ll.append(Node(4))
-# ll.append(Node(1))
-
-# print("Original Linked List:")
-# print_linked_list(ll)
-
-# sortedList = sort_linked_list(ll)
-
-# print("Sorted Linked List:")
-# print_linked_list(sortedList)
